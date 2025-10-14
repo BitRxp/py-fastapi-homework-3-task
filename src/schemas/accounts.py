97c776606a574902c8e6023e879947f7b11d40dc
bitRxp <oleksandrtaranenko239@gmail.com>
2025-10-14 19:56:47 +0300

    add account schemas

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,62 @@
+import re
+
 from pydantic import BaseModel, EmailStr, field_validator
 
 from database import accounts_validators
 
 
-# Write your code here
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("password")
+    def password_complexity(cls, password: str) -> str:
+        return accounts_validators.validate_password_strength(password)
+
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+    password: str
+
+    @field_validator("password")
+    def password_complexity(cls, v: str):
+        return accounts_validators.validate_password_strength(v)
+
+
+class UserLoginRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
